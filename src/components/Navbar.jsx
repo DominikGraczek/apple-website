e9509504ec6183a77580d2b3c48acce5e327ee85
DominikGraczek <dominik.graczek@gmail.com>
2024-11-09 10:55:04 +0100

    09.11.2024

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { appleImg } from '../utils';
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src={appleImg} alt='apple' width={14} height={18}/>
+        </nav>
+    </header>
+    )
+}
+
+export default Navbar;
